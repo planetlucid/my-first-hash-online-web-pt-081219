f00a82c028859f88fcbfab357ed28de631b96787
Ryan S <planetlucid@gmail.com>
2019-08-24 23:37:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,5 @@
 def create_hash
- 
  this_hash{"name" => "Dwayne, The Rock, Johnson", "weight" => "a lot"}
- # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
 end
 
 
