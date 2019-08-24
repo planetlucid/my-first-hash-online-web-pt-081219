aab013e57a88ec91a1718e3df16c20f9a5327df2
Ryan S <planetlucid@gmail.com>
2019-08-24 23:26:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,7 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+def my_hash =
+ 
+ {"name" => "Dwayne, The Rock, Johnson", "weight" => "a lot"}
+ # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
 end
